b51e690f6eb7528bc110f7e074fbdbb8e8659c53
Ayush Laware <ayushlaware@gmail.com>
2025-08-13 01:32:27 +0530

    APP.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import LoginButton from './utils/LoginButton'
+import LogoutButton from './utils/LogoutButton'
+import Profile from './utils/Profile'
 
+function App() {
   return (
-    <>
-      
-    </>
+    <div style={{ padding: '20px' }}>
+      <h1>Auth0 + React (Vite) Example</h1>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
   )
 }
 
